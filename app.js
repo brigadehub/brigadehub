728953cec73b6c0a346e03a0ec8306ddf2a5cdf6
Trent Oswald <trentoswald@therebelrobot.com>
2016-03-15 09:43:17 -0700

    feat(meetup): added meetup auth to passport/user account page
    see https://github.com/sfbrigade/brigadehub/pull/80#issuecomment-196393424

--- app.js
+++ app.js
@@ -228,10 +228,6 @@ app.post('/users/:userId/sync', passportConf.isAuthenticated, usersCtrl.postUser
 /**
  * OAuth authentication routes. (Sign in)
  */
-app.get('/auth/facebook', passport.authenticate('facebook', { scope: ['email', 'user_location'] }))
-app.get('/auth/facebook/callback', passport.authenticate('facebook', { failureRedirect: '/login' }), function (req, res) {
-  res.redirect(req.session.returnTo || '/')
-})
 app.get('/auth/github', passport.authenticate('github', {
   scope: [
     'user',
@@ -248,39 +244,12 @@ app.get('/auth/github', passport.authenticate('github', {
 app.get('/auth/github/callback', passport.authenticate('github', { failureRedirect: '/login' }), function (req, res) {
   res.redirect(req.session.returnTo || '/')
 })
-app.get('/auth/google', passport.authenticate('google', { scope: 'profile email' }))
-app.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/login' }), function (req, res) {
-  res.redirect(req.session.returnTo || '/')
-})
-app.get('/auth/twitter', passport.authenticate('twitter'))
-app.get('/auth/twitter/callback', passport.authenticate('twitter', { failureRedirect: '/login' }), function (req, res) {
-  res.redirect(req.session.returnTo || '/')
-})
-app.get('/auth/linkedin', passport.authenticate('linkedin', { state: 'SOME STATE' }))
-app.get('/auth/linkedin/callback', passport.authenticate('linkedin', { failureRedirect: '/login' }), function (req, res) {
-  res.redirect(req.session.returnTo || '/')
+app.get('/auth/meetup', passport.authenticate('meetup', { scope: ['basic', 'rsvp']}))
+app.get('/auth/meetup/callback', passport.authenticate('meetup', { failureRedirect: '/account' }), function (req, res) {
+  res.redirect(req.session.returnTo || '/account')
 })
 
-/**
- * OAuth authorization routes. (API examples)
- */
-app.get('/auth/foursquare', passport.authorize('foursquare'))
-app.get('/auth/foursquare/callback', passport.authorize('foursquare', { failureRedirect: '/api' }), function (req, res) {
-  res.redirect('/api/foursquare')
-})
-app.get('/auth/tumblr', passport.authorize('tumblr'))
-app.get('/auth/tumblr/callback', passport.authorize('tumblr', { failureRedirect: '/api' }), function (req, res) {
-  res.redirect('/api/tumblr')
-})
-app.get('/auth/venmo', passport.authorize('venmo', { scope: 'make_payments access_profile access_balance access_email access_phone' }))
-app.get('/auth/venmo/callback', passport.authorize('venmo', { failureRedirect: '/api' }), function (req, res) {
-  res.redirect('/api/venmo')
-})
-app.get('/auth/steam', passport.authorize('openid', { state: 'SOME STATE' }))
-app.get('/auth/steam/callback', passport.authorize('openid', { failureRedirect: '/login' }), function (req, res) {
-  res.redirect(req.session.returnTo || '/')
-})
-
+app.get('/auth/disconnect/:service', passportConf.isAuthenticated, usersCtrl.disconnectService)
 /**
  * Error Handler.
  */
