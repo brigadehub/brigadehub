dcef78c5642c11f12f628d804097a503a26cf1b7
spiffysparrow <tracykmullen@gmail.com>
2016-03-31 11:38:21 -0700

    added projects to homepage

--- controllers/home.js
+++ controllers/home.js
@@ -2,10 +2,28 @@
  * GET /
  * Home page.
  */
+
+ var Projects = require('../models/Projects')
+
+ var NUM_PROJECTS_SHOWN = 6;
+
 exports.index = function (req, res) {
   console.log(req.user)
+  Projects.find({brigade: res.locals.brigade.slug}, function (err, foundProjects) {
+    if (err) console.error(err)
+    var allKeywords = []
+    foundProjects.forEach(function (project) {
+      project.keywords.forEach(function (keyword) {
+        if (allKeywords.indexOf(keyword) < 0) {
+          allKeywords.push(keyword)
+        }
+      })
+    })
+
   res.render(res.locals.brigade.theme.slug + '/views/home', {
     title: 'Home',
-    brigade: res.locals.brigade
+    brigade: res.locals.brigade,
+    projects: foundProjects.splice(0, NUM_PROJECTS_SHOWN)
+  })
   })
 }
