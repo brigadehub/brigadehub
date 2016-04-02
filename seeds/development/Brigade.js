7583ecb6657f74684ba1b868060aea0b15b51e38
Trent Oswald <trentoswald@therebelrobot.com>
2016-04-02 14:32:11 -0700

    feat(db): added db seeding methods to npm scripts
    see #157

---
+++ seeds/development/Brigade.js
@@ -0,0 +1,56 @@
+var moment = require('moment')
+var faker = require('faker')
+
+module.exports = function () {
+  var auditDate = moment().format('YYYY-MM-DD HH:mm:ss')
+  return [{
+    name: 'Code for Example',
+    slug: 'codeforexample',
+    recoveryEmail: 'admin@codeforexample.org',
+    location: {
+      general: '',
+      specific: '',
+      geo: ''
+    },
+    heroImage: faker.image.city(1080,399),
+    url: 'http://codeforexample.org',
+    github: 'codeforexample',
+    theme: {
+      slug: 'atl',
+      logo: '',
+      show: {
+        title: true,
+        events: true,
+        projects: true,
+        blog: true,
+        about: true,
+        login: true
+      }
+    },
+    copy: {
+      tagline: 'is a community searching for technological solutions to social challenges.',
+      description: "We're a bunch of civic-minded technologists, designers, and topic experts using our skills to improve Example and the world."
+    },
+    auth: {
+      github: {
+        clientId: process.env.GITHUB_ID,
+        clientSecret: process.env.GITHUB_SECRET
+      },
+      slack: {
+        clientId: '',
+        clientSecret: ''
+      },
+      meetup: {
+        consumerKey: process.env.MEETUP_KEY,
+        consumerSecret: process.env.MEETUP_SECRET
+      },
+      google: {
+        clientId: '',
+        clientSecret: ''
+      }
+    },
+    auditLog: [
+      'initial | ' + auditDate + ' | added default brigade details from `config/default-brigade.js`'
+    ]
+  }]
+}
---
+++ seeds/development/Posts.js
@@ -0,0 +1,51 @@
+
+var faker = require('faker')
+
+module.exports = [
+  {
+    "slug": "test-post",
+    "title": "Test Post",
+    "author": "therebelrobot",
+    "url": "",
+    "image": faker.image.city(1080,600),
+    "description": faker.lorem.sentence(30),
+    "content": "# Example post\n\nThis is a test post\n\n- one\n- two\n\ntest | test2\n---- | -----\ntester | tester2\n\n",
+    "date": "Wed Mar 30 2016 18:51:00 GMT-0700 (PDT)",
+    "unix": 1459389060,
+    "tags": [
+      "test tag",
+      "test tag 2"
+    ]
+  },
+  {
+    "slug": "test-post-2",
+    "title": "Test Post 2",
+    "author": "andrewmbacon",
+    "url": "",
+    "image": faker.image.people(1080, 600),
+    "content": "# Example post 2\n\nThis is a test post\n\n- one\n- two\n\ntest | test2\n---- | -----\ntester | tester2\n\n",
+    "description": faker.lorem.sentence(30),
+    "date": "Wed Mar 30 2016 18:51:00 GMT-0700 (PDT)",
+    "unix": 1459389060,
+    "tags": [
+      "test tag 4",
+      "test tag 2"
+    ]
+  },
+  {
+    "slug": "external-post-3",
+    "title": "External Post 3",
+    "author": "spiffysparrow",
+    "url": faker.internet.url(),
+    "image": faker.image.technics(1080, 600),
+    "content": "# Example post 3\n\nThis is a test post\n\n- one\n- two\n\ntest | test2\n---- | -----\ntester | tester2\n\n",
+    "description": faker.lorem.sentence(30),
+    "date": "Wed Mar 30 2016 18:51:00 GMT-0700 (PDT)",
+    "unix": 1459389060,
+    "tags": [
+      "test tag 3",
+      "test tag 4",
+      "external"
+    ]
+  }
+]
