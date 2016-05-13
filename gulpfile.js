df73a9a3fe0d5993a2be32e7258dfcfc882db62a
dbcjckyee <dbcjckyee@gmail.com>
2016-05-13 15:04:31 -0700

    create gulp command for launching nodemon app.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,14 @@
 var gulp = require('gulp')
+var exec = require('child_process').exec
+
 gulp.task('test', function () {
   console.log('test')
+})
+
+gulp.task('launch', function () {
+  exec('nodemon app.js', function (err, stdout, stderr) {
+    console.log(stdout)
+    console.log(stderr)
+    console.log(err)
+  })
 })
