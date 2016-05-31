66b7fbd3ee00d4b62667d9dd01b1b76af6472ce2
dbcjckyee <dbcjckyee@gmail.com>
2016-05-31 15:38:03 -0700

    feat(compile): add gulp script for updating css.
    updates main.css whenever main.scss is changed. adds dependencies to gulp-sass and gulp-sourcemaps.
    
    no

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,8 @@ var exec = require('child_process').exec
 var watch = require('gulp-watch') // eslint-disable-line
 var browserify = require('browserify')
 var source = require('vinyl-source-stream')
+var sass = require('gulp-sass')
+var sourcemaps = require('gulp-sourcemaps')
 
 gulp.task('launch', function () {
   exec('nodemon app.js', function (err, stdout, stderr) {
@@ -23,4 +25,22 @@ gulp.task('frontjscomp', function () {
     .pipe(gulp.dest('themes/codeforpoland/public/js/'))
 })
 
-gulp.task('default', ['launch', 'frontjswatch', 'frontjscomp'])
+gulp.task('csswatch', function () {
+  gulp.watch('themes/' + 'codeforpoland' + '/public/css/main.scss', ['csscomp', 'sourcemapcomp'])
+})
+
+gulp.task('csscomp', function () {
+  return gulp.src('themes/codeforpoland/public/css/main.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('themes/codeforpoland/public/css'))
+})
+
+gulp.task('sourcemapcomp', function () {
+  return gulp.src('themes/codeforpoland/public/css/main.scss')
+  .pipe(sourcemaps.init())
+  .pipe(sass().on('error', sass.logError))
+  .pipe(sourcemaps.write())
+  .pipe(gulp.dest('themes/codeforpoland/public/css'))
+})
+
+gulp.task('default', ['launch', 'frontjswatch', 'frontjscomp', 'csswatch', 'csscomp', 'sourcemapcomp'])
