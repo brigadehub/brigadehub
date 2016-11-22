7de3a378e3bae3f4e1bf2c10730c53d2b9e439b4
Trent Oswald <trentoswald@therebelrobot.com>
2016-11-21 19:56:01 -0800

    fix(blog): fixed blog issues

---
+++ helpers/combineMiddleware.js
@@ -0,0 +1,12 @@
+module.exports = function combineMiddleware(middlewareArray) {
+  return middlewareArray.reduce(function(a, b) {
+    return function(req, res, next) {
+      a(req, res, function(err) {
+        if (err) {
+          return next(err);
+        }
+        b(req, res, next);
+      });
+    };
+  });
+}
